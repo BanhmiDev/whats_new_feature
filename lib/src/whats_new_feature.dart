a76c5f993082a6364392703b877e33f853c8bc8c
Son Nguyen <business@banhmi.dev>
2025-01-09 12:57:09 +0100

    Update whats_new_feature.dart

--- lib/src/whats_new_feature.dart
+++ lib/src/whats_new_feature.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whats_new_feature/src/widgets/whats_new_feature_page.dart';
 import 'package:whats_new_feature/src/widgets/whats_new_feature_tile.dart';
 
