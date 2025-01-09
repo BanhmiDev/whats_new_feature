e08a16031e881c5158c6a0ab40666edd46132f43
Son Nguyen <business@banhmi.dev>
2025-01-09 12:55:47 +0100

    Update whats_new_feature.dart

--- lib/src/whats_new_feature.dart
+++ lib/src/whats_new_feature.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whats_new_feature/src/widgets/whats_new_feature_page.dart';
 import 'package:whats_new_feature/src/widgets/whats_new_feature_tile.dart';
@@ -10,10 +9,7 @@ import 'package:whats_new_feature/src/widgets/whats_new_feature_tile.dart';
 class WhatsNewFeature {
   /// {@macro whats_new_feature}
   const WhatsNewFeature();
-
-  /// to obtain previous installed version
-  static const String installedAppVersionKey = 'installed_app_version_key';
-
+  
   /// show whats new page
   Future<void> showWhatsNew(
     BuildContext context, {
@@ -21,49 +17,27 @@ class WhatsNewFeature {
     Function()? navigatedToWhatsNewPage,
     required List<WhatsNewFeatureTile> features,
     required String title,
-    bool showWhatsNewOnFirstInstall = false,
     Color buttonColor = Colors.amber,
     Duration delay = const Duration(seconds: 1),
   }) async {
     // do not navigate to whats new page if [showWhatsNew] false
     if (showWhatsNew == false) return;
 
-    final packageInfo = await PackageInfo.fromPlatform();
-
-    final prefs = await SharedPreferences.getInstance();
-
-    final currentAppVersion = packageInfo.version;
-
-    final previousAppVersion = prefs.getString(installedAppVersionKey) ?? '';
-
-    /// do not show whats new feature page on the first install
-    ///
-    /// on the first install, [previousAppVersion] will always be empty
-    if (previousAppVersion.isEmpty && showWhatsNewOnFirstInstall == false) {
-      await prefs.setString(installedAppVersionKey, currentAppVersion);
-      return;
-    }
-
-    if (currentAppVersion != previousAppVersion) {
-      await prefs.setString(installedAppVersionKey, currentAppVersion);
-      navigatedToWhatsNewPage?.call();
-      Future.delayed(delay, () {
-        _navigateToWhatsNewPage(
-          context,
-          features,
-          title,
-          packageInfo.appName,
-          buttonColor,
-        );
-      });
-    }
+    navigatedToWhatsNewPage?.call();
+    Future.delayed(delay, () {
+      _navigateToWhatsNewPage(
+        context,
+        features,
+        title,
+        buttonColor,
+      );
+    });
   }
 
   void _navigateToWhatsNewPage(
     BuildContext context,
     List<WhatsNewFeatureTile> features,
     String title,
-    String appName,
     Color? buttonColor,
   ) {
     Navigator.push<void>(
@@ -72,8 +46,7 @@ class WhatsNewFeature {
         builder: (context) => WhatsNewFeaturePage(
           title: title,
           buttonColor: buttonColor,
-          features: features,
-          appName: appName,
+          features: features
         ),
       ),
     );
